f18ac81285578c760e44516827c1c837756e6b04
tiantian <tiantiansauce@gmail.com>
2024-10-19 19:48:23 +0800

    第一次使用

---
+++ hello.c
@@ -0,0 +1,13 @@
+//
+// Created by IRONMAN on 24-10-19.
+//
+#include<stdio.h>
+void printout() {
+    //edit your code here
+    printf("hello world");  // 添加分号
+}
+
+int main() {
+    printout();
+    return 0;
+}
